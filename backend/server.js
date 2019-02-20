f42812dc2519fbae61d3c16d9b9e0318ff544228
aaronBurns59 <aaronburns59@gmail.com>
2019-02-20 17:23:03 +0000

    added a model and created a schema for stock Stock

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,20 @@ var mongoose = require('mongoose');
 var mongoDB = 'mongodb://admin:admin1@ds149138.mlab.com:49138/store'
 mongoose.connect(mongoDB);
 
-//Generate a model in the src folder
+var Schema = mongoose.Schema;
+//A schema used for handling data with the stock model
+var stockSchema = new Schema
+({
+    price:number,
+    size:number,
+    colour:String,
+    brand:String,
+    material:String,
+})//stockSchema
+
+//The StockModel uses the collection in the DB and the schema created above
+var StockModel = mongoose.model('stock', stockSchema);
+
+//configuring express to use body-parser as middle-ware. 
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
---
+++ src/app/stock.ts
@@ -0,0 +1,8 @@
+export interface Stock
+{
+    price:number;
+    size:number;
+    colour:string;
+    brand:string;
+    material:string;
+}//stock
