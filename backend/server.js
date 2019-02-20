c7b2902b86352d4b3344dd2b9c38511ef303cd79
aaronBurns59 <aaronburns59@gmail.com>
2019-02-20 15:13:20 +0000

    Added the code that the mongoDB needs to connect to server

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+
+//Code to Access the mongoose database
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://admin:admin1@ds149138.mlab.com:49138/store'
+mongoose.connect(mongoDB);
+
+//Generate a model in the src folder
